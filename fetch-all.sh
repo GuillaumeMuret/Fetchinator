c215456eb1355af2b5a96a9871b8ad213f34d9bb
Guillaume MURET <guillaume.muret@niji.fr>
2023-07-15 11:55:49 +0200

    Add pdf generator

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUTPUT_DIR_NAME=fetched
+OUTPUT_DIR=${SCRIPT_DIR}/${OUTPUT_DIR_NAME}
+OUTPUT_PDF_DIR=${SCRIPT_DIR}/pdf
+
 get_prefixed_number() {
     TO_RETURN=$1
     if [[ $TO_RETURN -lt 10 ]]; then
@@ -12,26 +17,35 @@ get_prefixed_number() {
     echo $TO_RETURN
 }
 
+clean() {
+    rm -rf ${SCRIPT_DIR}/${OUTPUT_DIR_NAME}
+    rm -rf ${SCRIPT_DIR}/pdf
+}
+
 show_help() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  -h  Show help"
-    echo "  -r  Run the script"
-    echo ""
-    echo "For a correct script execution, you have to export some variables:"
-    echo "export BASE_URL_PREFIX_1=\"https://your-base-url/prefix-\""
-    echo "export BASE_URL_PREFIX_2=\"https://your-base-url/prefix-\""
-    echo "export BASE_URL_SUFFIX=\"/\""
-    echo "export MAX_ITERATOR=200"
-    echo ""
-    echo "The url fetched will be BASE_URL_PREFIX_1 + iterator + BASE_URL_SUFFIX"
-    echo ""
-    echo "Once everything is Downloaded, you can launch a server to view all photos"
-    echo ""
-    echo "python3 -m http.server"
-    echo ""
-    echo "And then open the display.html from the browser"
-    echo "open http://localhost:8000/display.html"
+    echo "
+    Usage: $0 [options]
+    Options:
+      -c  Clean: remove ${OUTPUT_DIR_NAME} and pdf dir
+      -h  Show help
+      -p  Generate pdf
+      -r  Run the script
+    
+    For a correct script execution, you have to export some variables:
+    export BASE_URL_PREFIX_1=\"https://your-base-url/prefix-\"
+    export BASE_URL_PREFIX_2=\"https://your-base-url/prefix-\"
+    export BASE_URL_SUFFIX=\"/\"
+    export MAX_ITERATOR=200
+    
+    The url fetched will be BASE_URL_PREFIX_1 + iterator + BASE_URL_SUFFIX
+    
+    Once everything is Downloaded, you can launch a server to view all photos
+    
+    python3 -m http.server
+    
+    And then open the display.html from the browser
+    open http://localhost:8000/display.html
+    "
 }
 
 check_env() {
@@ -67,7 +81,9 @@ fetch_chapter_num() {
         NEW_DIV=https$(echo "${DIV#*https}")
         LINK=$(echo "${NEW_DIV%%\"*}")
         NUM_CAPTURE=$(get_prefixed_number $ITERATOR_LINK)
-        curl -s $LINK --output "./fetched/${CHAPTER_NUM}-${NUM_CAPTURE}.jpg"
+        RELATIVE_OUTPUT_FILE="${CHAPTER_NUM}-${NUM_CAPTURE}.jpg"
+        OUTPUT_FILE="${OUTPUT_DIR}/${RELATIVE_OUTPUT_FILE}"
+        curl -s $LINK --output "${OUTPUT_FILE}"
         ITERATOR_LINK=$((ITERATOR_LINK+1))
     done
 }
@@ -75,27 +91,55 @@ fetch_chapter_num() {
 run_script() {
     echo "Running the script..."
     check_env
-    mkdir -p fetched
+    mkdir -p ${OUTPUT_DIR}
     for ((ITERATOR_CHAPTER=0; ITERATOR_CHAPTER<=${MAX_ITERATOR}; ITERATOR_CHAPTER++)); do
         CHAPTER_NUM=$(get_prefixed_number ${ITERATOR_CHAPTER})
         echo $CHAPTER_NUM
-        LIST=$(ls -lah ./fetched | grep "${CHAPTER_NUM}-")
+        LIST=$(ls -lah ${OUTPUT_DIR} | grep "${CHAPTER_NUM}-")
         if [[ -z $LIST ]] ; then
             fetch_chapter_num ${CHAPTER_NUM} ${BASE_URL_PREFIX_1} ${ITERATOR_CHAPTER}
         fi
-        LIST=$(ls -lah ./fetched | grep "${CHAPTER_NUM}-")
+        LIST=$(ls -lah ${OUTPUT_DIR} | grep "${CHAPTER_NUM}-")
         if [[ -z $LIST ]] ; then
             echo "ERROR BASE_URL_PREFIX_1 $CHAPTER_NUM" >> error.txt
             fetch_chapter_num ${CHAPTER_NUM} ${BASE_URL_PREFIX_2} ${ITERATOR_CHAPTER}
         fi
-        LIST=$(ls -lah ./fetched | grep "${CHAPTER_NUM}-")
+        LIST=$(ls -lah ${OUTPUT_DIR} | grep "${CHAPTER_NUM}-")
         if [[ -z $LIST ]] ; then
             echo "ERROR BASE_URL_PREFIX_2 $CHAPTER_NUM" >> error.txt
         fi
     done
 }
 
-while getopts ":hr" option; do
+pdf() {
+    IMAGES_HTML_TO_ADD=""
+    CHAPTER_FROM=$(get_prefixed_number 0)
+    mkdir -p ${OUTPUT_PDF_DIR}
+    for ((ITERATOR_CHAPTER=0; ITERATOR_CHAPTER<=${MAX_ITERATOR}; ITERATOR_CHAPTER++)); do
+        CHAPTER_NUM=$(get_prefixed_number ${ITERATOR_CHAPTER})
+        LIST=$(ls ${OUTPUT_DIR} | grep ${CHAPTER_NUM}-)
+        if [[ -z $LIST ]]; then
+            echo "ERROR NOTHING FOR CHAPTER $CHAPTER_NUM" >> error.txt
+        else
+            IFS=$'\n'
+            for IMAGES in $LIST; do
+                OUTPUT_FILE="${OUTPUT_DIR}/${RELATIVE_OUTPUT_FILE}"
+                IMAGES_HTML_TO_ADD="${IMAGES_HTML_TO_ADD}<img src=\"./${OUTPUT_DIR_NAME}/${IMAGES}\">"
+            done
+
+            if [[ $(( ITERATOR_CHAPTER % 3 )) -eq 0 || ${ITERATOR_CHAPTER} -eq ${MAX_ITERATOR} ]]; then
+                OUTPUT_HTML=display_${CHAPTER_FROM}-${CHAPTER_NUM}.html
+                sed "s+IMAGES_HTML_TO_ADD+${IMAGES_HTML_TO_ADD}+g" display.template.html > ${OUTPUT_HTML}
+                wkhtmltopdf -T 0 -B 0 --page-height 1000mm http://localhost:8000/${OUTPUT_HTML} ${OUTPUT_PDF_DIR}/display_${CHAPTER_FROM}-${CHAPTER_NUM}.pdf &
+                NEXT_CHAPTER=$(( ITERATOR_CHAPTER + 1 ))
+                CHAPTER_FROM=$(get_prefixed_number ${NEXT_CHAPTER})
+                IMAGES_HTML_TO_ADD=""
+            fi
+        fi
+    done
+}
+
+while getopts ":chpr" option; do
     case "$option" in
         h)
             show_help
@@ -103,6 +147,14 @@ while getopts ":hr" option; do
             ;;
         r)
             run_script
+            exit 0
+            ;;
+        p)
+            pdf
+            exit 0
+            ;;
+        c)
+            clean
             exit 0
             ;;
         \?)
